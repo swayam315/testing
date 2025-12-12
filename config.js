c648cc6697e9da8ed4e3e3cad1fe019c6f9167ee
podcraftindia <podcraftindia@gmail.com>
2025-12-12 09:56:40 +0000

    Changes before Firebase Studio auto-run

---
+++ config.js
@@ -0,0 +1,11 @@
+
+const GEMINI_API_KEY = "AIzaSyB2EoGXF8tt6vhzrpxYXOzmjJQqk_QFRBs";
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCK21ktDn0u_rlM6F7sdwJ5dUuolaq0LzU",
+    authDomain: "mockify-ai-f3a76.firebaseapp.com",
+    projectId: "mockify-ai-f3a76",
+    storageBucket: "mockify-ai-f3a76.firebasestorage.app",
+    messagingSenderId: "127093116858",
+    appId: "1:127093116858:web:066e97d5dce393a6ec5783",
+    measurementId: "G-71HZ8VWX4P"
+};
